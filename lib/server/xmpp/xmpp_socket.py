2a6af0272b9e301ac13c93d1081907f57a93bb1c
Kili <kilian.traub+github@mailbox.org>
2017-11-18 21:23:41 +0100

    fixid error in xmpp_socket

--- lib/server/xmpp/xmpp_socket.py
+++ lib/server/xmpp/xmpp_socket.py
@@ -7,26 +7,30 @@ class xmpp_socket(object):
     def __init__(self, hostname, port):
         self.hostname = hostname
         self.port = port
-        self.sock = socket(socket.AF_INET)
+        self.sock = socket.socket(socket.AF_INET)
         self.context = ssl.create_default_context()
+        self.connect()
 
-    @staticmethod
+    def connect(self):
+        self.sock.connect((self.hostname, self.port))
+
+    def close(self):
+        self.sock.close()
+
     def sendMessage(self, message):
         self.sock.send(message.encode())
 
-    @staticmethod
     def recvMessage(self):
         temp = self.sock.recv(4096).decode()
         print(temp)
         return temp
 
-    @staticmethod
     def upgradetoTLS(self):
         self.context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         self.context.verify_mode = ssl.CERT_REQUIRED
         self.context.check_hostname = True
         self.context.load_verify_locations("/etc/pki/tls/certs/ca-bundle.crt")
-        self.sock = self.context.wrap_socket(self.sock, self.hostname)
+        self.sock = self.context.wrap_socket(self.sock,server_hostname=self.hostname)
         return self.sock
 
 
