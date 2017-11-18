0a865b6f69e970d4061bbc5c47659e8f59b994cd
Kili <kilian.traub+github@mailbox.org>
2017-11-18 22:47:48 +0100

    fixing threading in App server

--- lib/server/app/appServer.py
+++ lib/server/app/appServer.py
@@ -1,19 +1,20 @@
 import socket
 from lib.log import log
-from threading import *
+from threading import Thread
 
 
 class appServer(object):
-    def __init__(self, hostname, logg):
+
+    def __init__(self, hostname, log):
         self.sock = socket.socket(socket.AF_INET)
         self.hostname = hostname
-        self.log = logg
+        self.log = log
         self.active = False
         self.connect()
 
     def connect(self):
         try:
-            self.sock.bind(address=(self.hostname, 6000))
+            self.sock.bind(('', 6000))
         except socket.error as e:
             self.log.printError("appserver bind failed. Error Code: "+str(e[0]) + " message: " + str(e[1]))
         self.log.printMessage("appserver socket binded")
@@ -33,5 +34,5 @@ class appServer(object):
     def loop(self):
         while self.active:
             conn, addr = self.sock.accept()
-            start_new_thread(self.processClient, (conn,))
+            Thread(target=self.processClient, args=(conn,)).start()
         self.sock.close()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from lib.log import log
-from lib.server.app import appServer
+from lib.server.app.appServer import appServer
 
 def main():
     import sys
     print(sys.version)
     logger = log("log", True)
-    Server = appServer("w215-5a-v4.eduroam.dynamic.rbg.tum.de", log)
-    appServer.loop()
+    Server = appServer("", logger)
+    Server.loop()
 
 
 
