9f8a4f7c030f64ac4053ff57a945844049b66639
Kili <kilian.traub+github@mailbox.org>
2017-11-18 13:12:25 +0100

    added wns send notification

--- lib/server/wns_connector.py
+++ lib/server/wns_connector.py
@@ -1,12 +1,14 @@
 from lib.server.wnslib import WNSClient
 from lib.log import log
+from lib.clientmanager import Clientmanager
 
 
 class wns_connector(object):
     token = None
 
-    def __init__(self, log):
+    def __init__(self, log, clients):
         self.log = log
+        self.client = clients
         try:
             with open("key.key", "r") as keyFile:
                 self.token = keyFile.readline()
@@ -14,5 +16,7 @@ class wns_connector(object):
             self.log.printError("no key file found (key.key), terminating!")
             exit(-1)
 
-    def sendNotification(wns_id, message):
-        pass
+    def sendNotification(self, wns_id, message):
+        push = {"type": 'toast', "text": [message]}
+        wnssession = WNSClient(wnsclientid=wns_id, wnsclientsecret=self.client.getWnsSecret(wns_id))
+        wnssession.process(token=self.token, message=push)
