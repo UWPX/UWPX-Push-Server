34d4e8f1c73d26010cb26c81af6ea62072d2e041
Kili <kilian.traub+github@mailbox.org>
2017-11-18 17:28:01 +0100

    upstream changes

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from lib.log import log
 from lib.server.wns_connector import wns_connector
+from concurrent import futures
+
 
 def main():
     import sys
     print(sys.version)
     logger = log("log", True)
     wns = wns_connector(logger)
+
+    with futures.ProcessPoolExecutor(4) as worker:
+        wns = wns_connector(logger)
+        while True:
+            pass
 
 
 
