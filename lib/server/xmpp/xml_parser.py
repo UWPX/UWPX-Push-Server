1f4a21a5151cd6f662d7218b566a4cebb3db1440
Unknown <TimPelzl@gmx.de>
2017-11-19 11:53:47 +0100

    Fixing stupid error in xml parser

--- lib/server/xmpp/xml_parser.py
+++ lib/server/xmpp/xml_parser.py
@@ -6,7 +6,7 @@ class XML_Parser(object):
     xml_dom = None
 
     def __init__(self, xml_str):
-        self.xml_dom = minidom.parseString("<push clientId='sauter.fabian@zefix.xyz' chatId='kili@zefix.xyzsauter.fabian@zefix.xyz' key='someKey' wns_id='123' wns_secret='string'/>")
+        self.xml_dom = minidom.parseString(xml_str)
         # self.xml_dom = minidom.parse('C:\\Users\\Tim\\Desktop\\test.xml')
         # use minidom.parse(filename) to process files
 
@@ -24,19 +24,18 @@ class XML_Parser(object):
             if item is None or item == "NULL":
                 raise Exception('Error: Missing elememt in XML string!')
 
-        return var_dict
-        # (var_dict['client_id'], var_dict['push_channel'])
+        return (var_dict['client_id'], var_dict['push_channel'])
         # tupel body: see clientmanager.py
 
 
 if __name__ == '__main__':
-    # pass
+    pass
 
-    
-    teststr = "      <push clientId='sauter.fabian@mailbox.org' pushChannel='https://db5.not ify.windows.com/?token=AwYAAACyXwnMijF375zd8XnwRkbdDuPhdn5ODXFjlTbcLCgqa%2fhH%2bU4kA3JSjlelYJEJ1d0tfqN0HsgIKkyQoBVD%2frK3eOwGSutgqmsyJXEmt9%2bg4O1RrjSH3%2fujvyxvujwLv6nJhOZjxMIDp2o4hjB0jIGt' />"
+    """
+    teststr = "<push clientId='sauter.fabian@mailbox.org' pushChannel='https://db5.not ify.windows.com/?token=AwYAAACyXwnMijF375zd8XnwRkbdDuPhdn5ODXFjlTbcLCgqa%2fhH%2bU4kA3JSjlelYJEJ1d0tfqN0HsgIKkyQoBVD%2frK3eOwGSutgqmsyJXEmt9%2bg4O1RrjSH3%2fujvyxvujwLv6nJhOZjxMIDp2o4hjB0jIGt' />"
     parser = XML_Parser(teststr)
     push_dict = parser.pushToTupel()
     
     for x, y in push_dict.items():
         print(x + ": " + y)
-    
+    """
