a90bc85bc2b0deec9b6a79f2bd36d009aff9cff4
Unknown <TimPelzl@gmx.de>
2017-11-19 11:41:44 +0100

    Improvements

--- lib/server/xmpp/xml_parser.py
+++ lib/server/xmpp/xml_parser.py
@@ -7,15 +7,15 @@ class XML_Parser(object):
 
     def __init__(self, xml_str):
         self.xml_dom = minidom.parseString("<push clientId='sauter.fabian@zefix.xyz' chatId='kili@zefix.xyzsauter.fabian@zefix.xyz' key='someKey' wns_id='123' wns_secret='string'/>")
+        # self.xml_dom = minidom.parse('C:\\Users\\Tim\\Desktop\\test.xml')
         # use minidom.parse(filename) to process files
 
     def pushToTupel(self):
         # Search for tag 'push'
-        push = self.xml_dom.getElementsByTagName("push")[0]
+        push = self.xml_dom.getElementsByTagName('push')[0]
 
         var_dict = dict()
         # Write values of attributes to dictionary
-        var_dict['wns_id'] = push.attributes['wns'].value
         var_dict['client_id'] = push.attributes['clientId'].value
         var_dict['push_channel'] = push.attributes['pushChannel'].value
 
@@ -23,18 +23,20 @@ class XML_Parser(object):
         for item in var_dict:
             if item is None or item == "NULL":
                 raise Exception('Error: Missing elememt in XML string!')
-               
-        return (var_dict['wns_id'], var_dict['client_id'], var_dict['push_channel'])
+
+        return var_dict
+        # (var_dict['client_id'], var_dict['push_channel'])
         # tupel body: see clientmanager.py
 
 
 if __name__ == '__main__':
-    pass
+    # pass
 
-    """
-    teststr = "<push clientId='sauter.fabian@zefix.xyz' chatId='kili@zefix.xyzsauter.fabian@zefix.xyz' key='someKey' wns_id=123 wns_secret='string'/>"
+    
+    teststr = "      <push clientId='sauter.fabian@mailbox.org' pushChannel='https://db5.not ify.windows.com/?token=AwYAAACyXwnMijF375zd8XnwRkbdDuPhdn5ODXFjlTbcLCgqa%2fhH%2bU4kA3JSjlelYJEJ1d0tfqN0HsgIKkyQoBVD%2frK3eOwGSutgqmsyJXEmt9%2bg4O1RrjSH3%2fujvyxvujwLv6nJhOZjxMIDp2o4hjB0jIGt' />"
     parser = XML_Parser(teststr)
-    push_dict = parser.pushNotification()
+    push_dict = parser.pushToTupel()
+    
     for x, y in push_dict.items():
         print(x + ": " + y)
-    """
+    
