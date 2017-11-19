9879dc8d9017091b1d75963d710c873e8eacc8ee
Kili <kilian.traub+github@mailbox.org>
2017-11-19 10:55:41 +0100

    moved appServer socket functionality to superclass

--- lib/server/app/appServer.py
+++ lib/server/app/appServer.py
@@ -1,30 +1,10 @@
-import socket
-from lib.log import log
-from threading import Thread
+from lib.server.raw_server import raw_Server
 from lib.server.xmpp.xml_parser import XML_Parser
 
-class appServer(object):
+class appServer(raw_Server):
 
     def __init__(self, hostname, log):
-        self.sock = socket.socket(socket.AF_INET)
-        self.hostname = hostname
-        self.log = log
-        self.active = False
-        self.connect()
-
-    def connect(self):
-        try:
-            self.sock.bind(('', 6000))
-        except socket.error as e:
-            self.log.printError("appserver bind failed.")
-            exit(-1)
-        self.log.printMessage("appserver successfuly added webserver")
-        self.sock.listen(10)
-        self.active = True
-
-    def close(self):
-        self.active = False
-        self.log.printMessage("closeing appserver, requested by other class")
+        super(appServer, self).__init__(hostname, log, 6000)
 
     def processClient(self, conn):
         input = conn.recv(1024)
@@ -35,9 +15,3 @@ class appServer(object):
             print(x + ": " + y)
         conn.sendall(b'success')
         conn.close()
-
-    def loop(self):
-        while self.active:
-            conn, addr = self.sock.accept()
-            Thread(target=self.processClient, args=(conn,)).start()
-        self.sock.close()
--- lib/server/raw_server.py
+++ lib/server/raw_server.py
@@ -1,29 +1,36 @@
-import ssl
 import socket
+from lib.log import log
+from threading import Thread
+
 
 class raw_Server(object):
-    """docstring for raw_Server"""
     def __init__(self, hostname, log, port):
-        super(raw_Server, self).__init__()
         self.sock = socket.socket(socket.AF_INET)
         self.hostname = hostname
         self.log = log
         self.port = port
         self.active = False
         self.connect()
-        
-    def __init__(self, hostname, log):
-        self.sock = socket.socket(socket.AF_INET)
-        self.hostname = hostname
-        self.log = log
-        self.active = False
-        self.connect()
 
     def connect(self):
         try:
-            self.sock.bind(('', 6000))
+            self.sock.bind(('', self.port))
         except socket.error as e:
-            self.log.printError("appserver bind failed. Error Code: "+str(e[0]) + " message: " + str(e[1]))
-        self.log.printMessage("appserver successfuly added webserver")
+            self.log.printError("server bind on Port: "+str(self.port)+" failed.")
+            exit(-1)
+        self.log.printMessage("server successfuly added webserver")
         self.sock.listen(10)
         self.active = True
+
+    def close(self):
+        self.active = False
+        self.log.printMessage("closeing appserver, requested by other class")
+
+    def processClient(self, conn):
+        raise NotImplementedError("raw_Server isn't ment to be used as standalone")
+
+    def loop(self):
+        while self.active:
+            conn, addr = self.sock.accept()
+            Thread(target=self.processClient, args=(conn,)).start()
+        self.sock.close()
