a8527665e7029e36685ba74e5398a82f1f3f886f
Kili <kilian.traub+github@mailbox.org>
2018-01-17 21:05:59 +0100

    improved pep8

--- lib/server/xmpp/xml_processor.py
+++ lib/server/xmpp/xml_processor.py
@@ -3,9 +3,8 @@ from xml.dom import minidom
 
 
 class xml_Processor(object):
-
-    def __init__(self, log, serverAddr):
-        self.log = log
+    def __init__(self, logger, serverAddr):
+        self.log = logger
         self.serverAddr = serverAddr
 
     def sessionEncrytable(xml):
@@ -22,12 +21,9 @@ class xml_generator(object):
         self.hostname = hostname
 
     def createXML(self, list):
-
-
-
         # Sample Code
-        XMLvalues = { 'fielda': "atesta", 'fieldb': "btestb"}
-        XMLvalues2 = { 'fielda22': "atesta22", 'fieldb22': "btestb22"}
+        XMLvalues = {'fielda': "atesta", 'fieldb': "btestb"}
+        XMLvalues2 = {'fielda22': "atesta22", 'fieldb22': "btestb22"}
         doc = minidom.Document()
         root = doc.createElement("User")
         root.setAttribute("id", 'myId')
@@ -35,16 +31,14 @@ class xml_generator(object):
         root.setAttribute("wns", "safajflafekfskf3593fesfsdf")
         doc.appendChild(root)
         for value in XMLvalues:
-           # Create Element
-           tempChild = doc.createElement(value)
-           tempChild.setAttribute("var", "11")
-           root.appendChild(tempChild)
-        
-           # Write Text
-           nodeText = doc.createTextNode(XMLvalues[value].strip())
-           tempChild.appendChild(nodeText)
-
-           for value2 in XMLvalues2:
+            # Create Element
+            tempChild = doc.createElement(value)
+            tempChild.setAttribute("var", "11")
+            root.appendChild(tempChild)
+            # Write Text
+            nodeText = doc.createTextNode(XMLvalues[value].strip())
+            tempChild.appendChild(nodeText)
+            for value2 in XMLvalues2:
                 tempChild2 = doc.createElement(value2)
                 tempChild2.setAttribute("var2", "2222")
                 tempChild.appendChild(tempChild2)
