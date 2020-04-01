28fbfccbf780f922ad6529b6a8d63e8b1302c8d3
Fabian Sauter <sauter.fabian@mailbox.org>
2020-04-01 15:58:24 +0200

    Fixed peewee datetime prsing with UTC offset

--- src/db/dbManager.py
+++ src/db/dbManager.py
@@ -11,29 +11,40 @@ https://docs.peewee-orm.com/en/latest/index.html
 DB_PATH: str = "pushServer.db"
 db: SqliteDatabase = SqliteDatabase(DB_PATH)
 
+# Required since SQLite stores datetime objects as string in a to peewee unknown format
+# in case they have an UTC offset.
+class DateTimeTZField(DateTimeField):
+    def python_value(self, value):
+        if value is None:
+            return
+        return datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f%z')
+
+    def db_value(self, value):
+        return value.strftime('%Y-%m-%d %H:%M:%S.%f%z') if value else None
+
 class BaseModel(Model):
     class Meta:
         database = db
 
 
 class ChannelUri(BaseModel):
-    deviceId: CharField = CharField(unique=True)
-    channelUri: CharField = CharField(null=False)
-    timeStamp: DateTimeField(default=datetime.now(timezone.utc), null=False)
+    deviceId: TextField = TextField(unique=True)
+    channelUri: TextField = TextField(null=False)
+    timeStamp: DateTimeTZField = DateTimeTZField(default=datetime.now(timezone.utc), null=False)
 
 
 class PushAccount(BaseModel):
     channelUri: ForeignKeyField(ChannelUri, backref="pushAccounts")
-    bareJidHash: CharField = CharField(null=False)
-    domainPart: CharField = CharField(null=False)
-    node: CharField = CharField(null=False)
-    secret: CharField = CharField(null=False)
+    bareJidHash: TextField = TextField(null=False)
+    domainPart: TextField = TextField(null=False)
+    node: TextField = TextField(null=False)
+    secret: TextField = TextField(null=False)
 
 
 class WNSTokenModel(BaseModel):
-    token: CharField = CharField(unique=True)
-    tokenType: CharField = CharField(unique=True)
-    expires: DateTimeField(default=datetime.now(timezone.utc), null=False)
+    token: TextField = TextField(unique=True)
+    tokenType: TextField = TextField(unique=True)
+    expires: DateTimeTZField = DateTimeTZField(default=datetime.now(timezone.utc), null=False)
 
     @classmethod
     def createFrom(cls, channelUri: ChannelUri, bareJid: str):
