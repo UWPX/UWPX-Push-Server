a925928eab1a47bc5fd5ab14dd59f3e862042e4a
Fabian Sauter <sauter.fabian@mailbox.org>
2020-04-20 18:11:35 +0200

    Catching shutdown signals

--- src/main.py
+++ src/main.py
@@ -3,19 +3,33 @@
 from server import Server
 from time import sleep
 from config import loadConfig
+from signal import signal, SIGTERM, SIGILL, SIGINT
 
+shouldRun: bool = True
+
+def receiveSignal(signalNumber, frame):
+    print("Shutting down...")
+    shouldRun = False
+
+def initSignalHandler():
+    signal(SIGTERM, receiveSignal)
+    signal(SIGILL, receiveSignal)
+    signal(SIGINT, receiveSignal)
 
 def main():
+    initSignalHandler()
+
     server: Server = Server(loadConfig())
     try:
         server.start()
 
         # Wait for a keyboard interupt:
-        while True:
-            sleep(1)
+        while shouldRun:
+            sleep(0.5)
     except KeyboardInterrupt:
-        server.stop()
-        print("Server stopped by keyboard interrupt.")
+        print("Keyboard interupt received. Shutting down...")
+    server.stop()
+    print("Good bye.")
 
 
 if __name__ == '__main__':
