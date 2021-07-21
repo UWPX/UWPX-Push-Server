8b63e6490d0163f930e6589c1ddcc8af9f8ad246
Fabian Sauter <sauter.fabian@mailbox.org>
2021-07-21 09:34:15 +0200

    Fixed libstrophe for ninja build

--- cmake/libstrophe.cmake
+++ cmake/libstrophe.cmake
@@ -4,27 +4,21 @@ include(ExternalProject)
 
 # Disable linting for libstrophe
 clear_variable(DESTINATION CMAKE_CXX_CLANG_TIDY BACKUP CMAKE_CXX_CLANG_TIDY_BKP)
+set(LIBSTROPHE_LIBRARY_INSTALL_PATH ${CMAKE_BINARY_DIR}/strophe/lib/${CMAKE_FIND_LIBRARY_PREFIXES}strophe.so)
 ExternalProject_Add(libstrophe
     URL https://github.com/strophe/libstrophe/releases/download/0.10.1/libstrophe-0.10.1.tar.gz
     URL_HASH SHA256=4918c47029ecdea2deab4b0f9336ca4a8bb12c28b72b2cec397d98664b94c771
     SOURCE_DIR libstrophe-prefix/src/libstrophe-master
     BUILD_IN_SOURCE ON
-    CONFIGURE_COMMAND pwd
-        COMMAND ./bootstrap.sh
-        COMMAND ./configure
+    CONFIGURE_COMMAND ./bootstrap.sh
+              COMMAND ./configure --prefix ${CMAKE_BINARY_DIR}/strophe
     BUILD_COMMAND make
-    INSTALL_COMMAND ""
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON
-)
+    INSTALL_COMMAND make install
+    BUILD_BYPRODUCTS ${LIBSTROPHE_LIBRARY_INSTALL_PATH}) # This line fixes ninja not being able to build libstrophe
 ExternalProject_Get_Property(libstrophe source_dir)
-include_directories(
-    ${source_dir}/
-)
-ExternalProject_Get_Property(libstrophe binary_dir)
 add_library(strophe UNKNOWN IMPORTED)
-set_property(TARGET strophe PROPERTY IMPORTED_LOCATION ${binary_dir}/.libs/${CMAKE_FIND_LIBRARY_PREFIXES}strophe.so)
+target_include_directories(strophe INTERFACE ${CMAKE_BINARY_DIR}/strophe/include)
+set_property(TARGET strophe PROPERTY IMPORTED_LOCATION ${LIBSTROPHE_LIBRARY_INSTALL_PATH})
 add_dependencies(strophe libstrophe)
 
 restore_variable(DESTINATION CMAKE_CXX_CLANG_TIDY BACKUP CMAKE_CXX_CLANG_TIDY_BKP)
