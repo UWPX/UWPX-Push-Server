375e2523d2fc999a4874f3f5e4e1135feb65ec00
Fabian Sauter <sauter.fabian@mailbox.org>
2022-01-24 17:04:03 +0000

    Added a message when the ssl server stops

--- src/tcp/SslServer.cpp
+++ src/tcp/SslServer.cpp
@@ -7,7 +7,11 @@ namespace tcp {
 SslServer::SslServer(const std::shared_ptr<CppServer::Asio::Service>& asioService, const std::shared_ptr<CppServer::Asio::SSLContext>& sslCtx, CppServer::Asio::InternetProtocol protocol, uint16_t port, ClientSslSession::messageHandlerFunc&& messageHandler) : CppServer::Asio::SSLServer(asioService, sslCtx, port, protocol), messageHandler(std::move(messageHandler)) {}
 
 void SslServer::onError(int error, const std::string& category, const std::string& message) {
-    LOG_ERROR << "[" << id().string() << "] SSL session caught an error (code " << error << ") with category " << category << " and message: " << message;
+    LOG_ERROR << "[" << id().string() << "] SSL server caught an error (code " << error << ") with category " << category << " and message: " << message;
+}
+
+void SslServer::onStopped() {
+    LOG_ERROR << "[" << id().string() << "] SSL server stopped.";
 }
 
 std::shared_ptr<CppServer::Asio::SSLSession> SslServer::CreateSession(const std::shared_ptr<CppServer::Asio::SSLServer>& server) {
